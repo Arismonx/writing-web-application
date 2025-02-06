39ac0d688866af4637f796638143879885943351
Arismonx <arismonx@gmail.com>
2025-02-06 14:57:36 +0700

    Introducing Function Literals and Closures

--- net-http.go
+++ net-http.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 type Page struct {
@@ -12,11 +13,24 @@ type Page struct {
 	Body  []byte
 }
 
+var valitPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseFiles("edit.html", "view.html", "index.html"))
+
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
+// func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+// 	m := valitPath.FindStringSubmatch(r.URL.Path)
+
+// 	if m == nil {
+// 		http.NotFound(w, r)
+// 		return "", errors.New("invalid Page Title")
+// 	}
+
+//		return m[2], nil
+//	}
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename)
@@ -29,70 +43,70 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, s string, p *Page) {
-	t, err := template.ParseFiles(s + ".html")
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	err = t.Execute(w, p)
-
+	err := templates.ExecuteTemplate(w, s+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("index.html")
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	t, err := template.ParseFiles("index.html")
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, "")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	err = t.Execute(w, "")
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//		}
+//	}
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := valitPath.FindStringSubmatch(r.URL.Path)
+
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		fn(w, r, m[2])
 	}
 }
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
-
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
 	}
 
 	renderTemplate(w, "view", p)
 }
 
-func edithandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
-
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	renderTemplate(w, "edit", p)
 }
 
-func savehandler(w http.ResponseWriter, r *http.Request) {
-	titel := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
-	p := &Page{Title: titel, Body: []byte(body)}
+	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
-
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	http.Redirect(w, r, "/view/"+titel, http.StatusFound)
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", edithandler)
-	http.HandleFunc("/save/", savehandler)
+	// http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
